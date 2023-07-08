d51ce9e11324670c133fd70928b8389e4a6028b7
kambizAbdali <kambizAbdali@gmail.com>
2023-07-08 23:40:11 +0430

    Adding the PHPTRAVELS MS-Test project with classes and packages needed to work with Selenium

---
+++ MSTestProject/MSTestProject/ComponentHelper/NavigationHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+
+namespace SeleniumWebdriver.ComponentHelper
+{
+    public class NavigationHelper
+    {
+        private static readonly ILog Logger = Log4NetHelper.GetXmlLogger(typeof (NavigationHelper));
+
+        public static void NavigateToUrl(string Url)
+        {
+            ObjectRepository.Driver.Navigate().GoToUrl(Url);
+            Logger.Info(" Navigate To Page " + Url);
+            if (Url.StartsWith("https:"))
+                handleSSLError();
+        }
+
+        private static void handleSSLError() {
+            try
+            {
+                GenericHelper.WaitForWebElement(By.Id("details-button"), TimeSpan.FromSeconds(5));
+                IWebElement Advanced = ObjectRepository.Driver.FindElement(By.Id("details-button"));
+                Advanced.Click();
+
+                GenericHelper.WaitForWebElement(By.Id("proceed-link"), TimeSpan.FromSeconds(60));
+                IWebElement unsafeBtn = ObjectRepository.Driver.FindElement(By.Id("proceed-link"));
+                unsafeBtn.Click();
+            }
+            catch {
+
+            }
+        }
+    }
+}
---
+++ MSTestProject/MSTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d0c0dff-8084-4f3b-9152-7c61d9113f9a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
